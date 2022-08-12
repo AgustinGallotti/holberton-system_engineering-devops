070923d3f3f70c5c2dcaae086c0c4e6b33132c68
Agustin Gallotti <AgustinGallotti@gmail.com>
2022-08-12 14:02:43 -0700

    chek

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -4,6 +4,6 @@
 exec {'killmenow':
   command  => '/usr/bin/pkill',
   provider => 'procps-ng',
-  creates  => '/holbertonschool-system_engineering-devops/0x0A-configuration_management/killmenow',
+  path  => '/holbertonschool-system_engineering-devops/0x0A-configuration_management/killmenow',
   cwd      => '/0x0A-configuration_management',
 }

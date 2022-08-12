799a5e59199dc070a48b8877daa0891f9f400d1c
Agustin Gallotti <AgustinGallotti@gmail.com>
2022-08-12 14:12:28 -0700

    chek

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -3,7 +3,7 @@
 
 exec {'pkill':
   cwd      => '0x0A-configuration_management',
-  command  => '/usr/bin/pkill',
+  command  => '/usr/bin/pkill -f',
   provider => 'procps',
   creates  => '/holbertonschool-system_engineering-devops/0x0A-configuration_management/killmenow',
 }
